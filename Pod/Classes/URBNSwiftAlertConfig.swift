67a715a6316218fa991c1b8bcc90b81933118961
Kevin Taniguchi <kv.taniguchi@gmail.com>
2016-10-09 18:05:02 -0500

    config

--- Pod/Classes/URBNSwiftAlertConfig.swift
+++ Pod/Classes/URBNSwiftAlertConfig.swift
@@ -6,4 +6,46 @@
 //
 //
 
-import Foundation
+import UIKit
+
+class URBNSwiftAlertConfig: NSObject {
+    /**
+    *  Title text for the alert
+    */
+    var title = ""
+
+    /**
+    *  Message text for the alert
+    */
+    var message = ""
+
+    /**
+     *  Array of actions added to the alert
+     */
+    var actions = [URBNSwiftAlertAction]()
+
+    /**
+     *  Array of UITextFields added to the array
+     */
+    var inputs = [UITextField]()
+
+    /**
+     *  The view to present from when using showInView:
+     */
+    weak var presentationView: UIView?
+
+    /**
+     *  Flag if the alert is active. False = a passive alert
+     */
+    var isActiveAlert = true
+
+    /**
+     *  Duration of a passive alert (no buttons added)
+     */
+    var duration = 3
+
+    /**
+     *  When set to YES, you can touch outside of an alert to dismiss it
+     */
+    var shouldTouchOutsideToDismiss = true
+}
